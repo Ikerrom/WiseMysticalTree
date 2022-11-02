a14e28536c1d02a4cc13fca5d922b015c8290541
xabier <diaz.xabier@uni.eus>
2022-11-02 13:36:17 +0100

    Metodo CreateUser en views.py

--- wmtapp/views.py
+++ wmtapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
 
 def MainPage(request):
     return HttpResponseRedirect(reverse('MainPage'))
@@ -10,6 +11,16 @@ def Login(request):
 
 def GuessPage(request):
     return HttpResponseRedirect(reverse('GuessPage'))
+
+###
+def CreateUser(request):
+    #Superusuario
+    user = User.objects.create_user('myusername', 'myemail@crazymail.com', 'mypassword')
+    
+    user.first_name = 'John'
+    user.last_name = 'Citizen'
+    user.save()
+###
 
 def Carta(request):
     return HttpResponseRedirect(reverse('Carta'))
