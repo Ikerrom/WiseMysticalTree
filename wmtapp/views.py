550f9d21053cc2d173b882007d63121b082eec26
Ikerrom <55279037+Ikerrom@users.noreply.github.com>
2022-11-08 13:37:52 +0100

    Question Filter

--- wmtapp/views.py
+++ wmtapp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.http import HttpResponse, HttpResponseRedirect,JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib.auth.models import User
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
 import json
+
 
 def index(request):
     template = loader.get_template('index.html')
@@ -17,6 +18,8 @@ def login(request):
     return HttpResponse(template.render())
 
 ###
+
+
 def createuser(request):
     # Superusuario
     user = User.objects.create_user(
@@ -27,9 +30,11 @@ def createuser(request):
     user.save()
 ###
 
+
 def menu(request):
     template = loader.get_template('menu.html')
     return HttpResponse(template.render())
+
 
 @csrf_exempt
 def filterquestion(request):
@@ -37,20 +42,25 @@ def filterquestion(request):
     preferences = request.POST.get('preferences')
     categories = Category.objects.all()
 
-    categorylist =[]
+    filteredquestions = []
+    categorylist = []
     for category in categories:
         categorylist.append(category.categoryname)
-    
+
     intoleranceslist = json.loads(intolerances)
     preferenceslist = json.loads(preferences)
 
     for intolerance in intoleranceslist:
         intoleranceobj = Intolerance.objects.get(intolerancename=intolerance)
         intolerancecaregorylist = IntoleranceCategory.objects.filter(intolerance=intoleranceobj)
-        
+
         for intolerancecaregory in intolerancecaregorylist:
             categorylist.remove(intolerancecaregory.category.categoryname)
 
+    questioncategories = list(QuestionCategory.objects.all())
     
-
-    
+    for category in categorylist:
+        for questioncategory in questioncategories:
+            if category == questioncategory.category.categoryname:
+                filteredquestions.append(questioncategory.question)
+    print(filteredquestions)
